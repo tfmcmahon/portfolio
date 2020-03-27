c5aac91251c07bb9086fd3eab448dc4fbe5819a4
tfmcmahon <tfmcmahon@gmail.com>
2020-03-27 09:42:42 -0500

    Initial commit - base structure upload

---
+++ Portfolio site/js/index.js
@@ -0,0 +1,12 @@
+const navToggle = document.querySelector('.navToggle')
+const navLinks = document.querySelectorAll('.navLink')
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('navOpen')
+})
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('navOpen');
+    })
+})
